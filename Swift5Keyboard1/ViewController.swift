e78e11f9b20037e6ec3a563223fb897cb085fab5
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-22 20:07:15 +0900

    Initial Commit

---
+++ Swift5Keyboard1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5Keyboard1
+//
+//  Created by 野口晃 on 2020/06/22.
+//  Copyright © 2020 Akira Noguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
