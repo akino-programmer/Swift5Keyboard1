7dd93cbed9428ea0c986eeb6aa5485149f94ed50
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-24 06:29:49 +0900

    userName and passWord to label.

--- Swift5Keyboard1/ViewController.swift
+++ Swift5Keyboard1/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:
+   UIViewController,UITextFieldDelegate {
+   
     
     @IBOutlet weak var logoImageView: UIImageView!
     
@@ -17,17 +18,42 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var passWordTextField: UITextField!
     
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var passwordLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        userNameTextField.delegate = self
+        passWordTextField.delegate = self
+        
+        
     }
 
     @IBAction func login(_ sender: Any) {
         
         
+        logoImageView.image = UIImage(named: "loginOK")
         
+        userNameLabel.text = userNameTextField.text
+        passwordLabel.text = passWordTextField.text
+        
+        
+    }
+    
+    //タッチでキーボードを閉じる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    //リターンキーを押した時にキーボードを閉じる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+    textField.resignFirstResponder()
+
+    return true
+
     }
     
 }
